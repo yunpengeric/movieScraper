c5bbf1fac64a2b610f925b223bae662c812f6652
Eric <ericyi@hotmail.com>
2022-05-08 17:26:38 +1000

    Update movieScraper.py

--- bin/movieScraper.py
+++ bin/movieScraper.py
@@ -1,7 +1,8 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as bf
 def movieScraper():
-    req = Request("http://www.xiaopian.com/html/gndy/dyzz/index.html", headers={'User-Agent': 'Mozilla/5.0'})   
+    req = Request("http://www.xiaopian.com/html/gndy/dyzz/index.html", headers={'User-Agent': 'Mozilla/5.0'})
+    print(req)
     html = urlopen(req)
     print("html is", html)
     obj = bf(html.read().decode('gbk'),'html.parser')
