3b319c7068dab7f35ec59ba61e1e97af0ca8e06d
eric <ericyi@hotmail.com>
2022-05-30 22:24:14 +1000

    add gbk decoding

--- bin/movieScraper.py
+++ bin/movieScraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bf
 def movieScraper():
     page = requests.get("http://www.xiaopian.com/html/gndy/dyzz/index.html", headers={'User-Agent': 'Mozilla/5.0'})
-    html = bf(page.content, "html.parser")  
+    html = bf(page.content.decode("gbk"), "html.parser")  
     tables = html.find_all("a", class_="ulink")
     for i,table in enumerate(tables):
         title = table["title"]
