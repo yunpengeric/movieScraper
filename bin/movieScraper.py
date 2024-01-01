a1019643eab389bc533f88f4c3f8a96e7bf43361
eric <ericyi@hotmail.com>
2024-01-01 22:50:42 +1100

    chage to page content

--- bin/movieScraper.py
+++ bin/movieScraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bf
 def movieScraper():
     page = requests.get("https://m.xiaopian.com/html/gndy/dyzz/index.html", headers={'User-Agent': 'Mozilla/5.0'})
-    print(page.text)
+    print(page.content)
     html = bf(page.content.decode("gbk"), "html.parser")  
     tables = html.find_all("a", class_="ulink")
     for i,table in enumerate(tables):
