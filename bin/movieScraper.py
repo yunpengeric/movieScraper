382b13258a6ef4d7421cde91551e8728a038ebed
eric <ericyi@hotmail.com>
2024-01-01 22:45:30 +1100

    add print page.text

--- bin/movieScraper.py
+++ bin/movieScraper.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bf
 def movieScraper():
-    page = requests.get("https://www.xiaopian.com", headers={'User-Agent': 'Mozilla/5.0'})
-    print(page)
+    page = requests.get("https://m.xiaopian.com/html/gndy/dyzz/index.html", headers={'User-Agent': 'Mozilla/5.0'})
+    print(page.text)
     html = bf(page.content.decode("gbk"), "html.parser")  
     tables = html.find_all("a", class_="ulink")
     for i,table in enumerate(tables):
