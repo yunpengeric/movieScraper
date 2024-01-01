9537896a7aed23921dba8d15cf463be81d57fc7a
eric <ericyi@hotmail.com>
2024-01-01 23:07:23 +1100

    change the decode

--- bin/movieScraper.py
+++ bin/movieScraper.py
@@ -2,9 +2,8 @@ import requests
 from bs4 import BeautifulSoup as bf
 def movieScraper():
     page = requests.get("https://m.xiaopian.com/html/gndy/dyzz/index.html", headers={'User-Agent': 'Mozilla/5.0'})
-    page.encoding = 'utf-8'
-    print(page.content)
-    html = bf(page.content.decode("gbk"), "html.parser")  
+    html = bf(page.content.decode("utf-8"), "html.parser")  
+    print(html)
     tables = html.find_all("a", class_="ulink")
     for i,table in enumerate(tables):
         title = table["title"]
