3abfd5eb7ce26f2eb91e03e944339c2c47e08437
eric <ericyi@hotmail.com>
2024-01-02 09:00:32 +1100

    add headers

--- bin/movieScraper.py
+++ bin/movieScraper.py
@@ -1,7 +1,15 @@
 import requests
 from bs4 import BeautifulSoup as bf
 def movieScraper():
-    page = requests.get("http://m.xiaopian.com/html/gndy/dyzz/index.html", headers={'User-Agent': 'Chrome/120.0.6099.129'})
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36", 
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Language": "en-US,en;q=0.9",  
+        "Referer": "http://www.google.com", 
+        "DNT": "1",
+        "Upgrade-Insecure-Requests": "1"
+    }
+    page = requests.get("http://m.xiaopian.com/html/gndy/dyzz/index.html", headers=headers
     html = bf(page.content.decode("utf-8"), "html.parser")  
     print(html)
     tables = html.find_all("a", class_="ulink")
